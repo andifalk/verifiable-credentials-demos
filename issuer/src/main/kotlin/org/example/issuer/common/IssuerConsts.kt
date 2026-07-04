0ed8efd72bf6ff0bee422e6005fe21e42201c189
Andreas Falk <info@andreas-falk.de>
2026-07-04 15:51:40 +0200

    Remove old demos

--- issuer/src/main/kotlin/org/example/issuer/common/IssuerConsts.kt
+++
@@ -1,5 +0,0 @@
-package org.example.issuer.common
-
-object IssuerConsts {
-    const val BASE_URL = "http://localhost:9091"
-}
--- verifier/src/main/kotlin/org/example/verifier/VerifierConfigurationProperties.kt
+++
@@ -1,8 +0,0 @@
-package org.example.verifier
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import java.net.URI
-
-@ConfigurationProperties(prefix = "verifier")
-data class VerifierConfigurationProperties(val walletUrl: URI) {
-}
